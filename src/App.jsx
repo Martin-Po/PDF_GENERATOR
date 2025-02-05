7b0a1f9596971b55e62b3bb9793154d549ddb6d7
Martin-Po <85139462+Martin-Po@users.noreply.github.com>
2025-02-05 12:49:08 -0300

    Created proyect

---
+++ src/App.jsx
@@ -0,0 +1,289 @@
+import { useEffect, useState } from "react";
+import { TextField, Button, Container, Paper, Typography, Box, IconButton } from "@mui/material";
+import { QRCodeCanvas } from "qrcode.react";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import { useRef } from "react";
+import { generatePDF } from "./utils/generatePDF";
+
+
+function App() {
+  const [formData, setFormData] = useState({
+    id_usuario: "",
+    nombre: "",
+    apellido: "",
+    dni: "",
+    fecha_emision: "",
+    fecha_vencimiento: "",
+    imageUrl: "", // Store image URL
+    imageFile: null, // Store selected file
+  });
+
+  const [qrValue, setQrValue] = useState("");
+  const fileInputRef = useRef(null);
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    // When imageUrl changes, fetch the image
+    if (formData.imageUrl) {
+      fetchImage(formData.imageUrl);
+    }
+  }, [formData.imageUrl]); // This hook runs whenever imageUrl changes
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case "id_usuario":
+        if (!/^\d*$/.test(value)) return;
+        break;
+
+      case "nombre":
+        break;
+
+      case "apellido":
+        break;
+
+      case "dni":       
+        break;
+
+      case "fecha_emision": {
+        const date = new Date(value);
+        if (isNaN(date.getTime())) return;
+
+        const nextYear = new Date(date);
+        nextYear.setFullYear(date.getFullYear() + 1);
+
+        setFormData((prev) => ({
+          ...prev,
+          fecha_emision: value,
+          fecha_vencimiento: nextYear.toISOString().split("T")[0],
+        }));
+        return;
+      }
+
+      case "imageUrl":
+        // Trigger image URL change in the state
+        setFormData((prev) => ({ ...prev, imageUrl: value }));
+        break;
+
+      default:
+        break;
+    }
+
+    // Update other form data fields
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const fetchImage = async (imageUrl) => {
+    try {
+      const res = await fetch(imageUrl);
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }      
+      const imageBlob = await res.blob();
+      setFormData((prev) => ({
+        ...prev,
+        imageFile: URL.createObjectURL(imageBlob),
+      }));
+    } catch (error) {
+      console.log("Error fetching image:", error);
+    }
+  };
+  // Handle image file selection
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setFormData((prev) => ({ ...prev, imageFile: reader.result }));
+    };
+    reader.readAsDataURL(file);
+  };
+
+
+
+  const generateQR = () => {
+    console.log("Generating QR Code...");
+    
+    const dataString = `Nombre: ${formData.nombre}\nApellido: ${formData.apellido}\nDNI: ${formData.dni}`;
+    setQrValue(dataString);
+    return dataString;
+  };
+  
+  const validateForm = () => {
+    let newErrors = {};
+  
+    if (!formData.id_usuario) newErrors.id_usuario = "ID Usuario es requerido";
+    if (!formData.nombre) newErrors.nombre = "Nombre es requerido";
+    if (!formData.apellido) newErrors.apellido = "Apellido es requerido";
+    if (!formData.dni) 
+      {newErrors.dni = "DNI es requerido";}
+    else{
+      if (!/^\d{1,2}\.\d{3}\.\d{3}$/.test(formData.dni)) newErrors.dni = "Formato incorrecto. Debe ser xx.xxx.xxx";
+    }
+    if (!formData.fecha_emision) newErrors.fecha_emision = "Fecha de emisión es requerida";
+    if (!formData.fecha_vencimiento) newErrors.fecha_vencimiento = "Fecha de vencimiento es requerida";
+    if (!formData.imageFile) newErrors.imageFile = "Imagen es requerida";
+  
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+
+  const handleGeneratePDF = async () => {
+    if (validateForm()) {
+      const newQRValue = await generateQR(formData.id_usuario);
+      generatePDF(newQRValue, formData);
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Paper elevation={3} sx={{ p: 4, mt: 4, textAlign: "center" }}>
+        <Typography variant="h5" gutterBottom>
+          GENERAR PDF
+        </Typography>
+
+        <TextField
+          fullWidth
+          label="Id Usuario"
+          name="id_usuario"
+          value={formData.id_usuario}
+          onChange={handleChange}
+          margin="normal"
+          error={!!errors.id_usuario}
+          helperText={errors.id_usuario}
+        />
+        <TextField
+          fullWidth
+          label="Nombre"
+          name="nombre"
+          value={formData.nombre}
+          onChange={handleChange}
+          margin="normal"
+          error={!!errors.nombre}
+          helperText={errors.nombre}
+        />
+        <TextField
+          fullWidth
+          label="Apellido"
+          name="apellido"
+          value={formData.apellido}
+          onChange={handleChange}
+          margin="normal"
+          error={!!errors.apellido}
+          helperText={errors.apellido}
+        />
+        <TextField
+          fullWidth
+          label="DNI"
+          name="dni"
+          value={formData.dni}
+          onChange={handleChange}
+          margin="normal"
+          error={!!errors.dni}
+          helperText={errors.dni}
+        />
+
+        <Box sx={{ display: "flex", justifyContent: "space-between", gap: 2 }}>
+          <TextField
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            label="Fecha Emisión"
+            name="fecha_emision"
+            type="date"
+            value={formData.fecha_emision}
+            onChange={handleChange}
+            margin="normal"
+            error={!!errors.fecha_emision}
+            helperText={errors.fecha_emision}
+          />
+          <TextField
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            label="Fecha Vencimiento"
+            name="fecha_vencimiento"
+            type="date"
+            value={formData.fecha_vencimiento}
+            onChange={handleChange}
+            margin="normal"
+            error={!!errors.fecha_vencimiento}
+            helperText={errors.fecha_vencimiento}
+          />
+        </Box>
+
+
+        <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+
+          {/* Image URL */}
+          <TextField
+            fullWidth
+            label="Imagen URL"
+            name="imageUrl"
+            value={formData.imageUrl}
+            onChange={handleChange}
+            margin="normal"
+            error={!!errors.imageFile}
+            helperText={errors.imageFile}
+          />
+
+          <IconButton onClick={() => fileInputRef.current.click()}>
+            <FileUploadIcon />
+          </IconButton>
+          <input
+            type="file"
+            accept="image/*"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+            style={{ display: "none" }} // Hide the input
+          />
+        </Box>
+        {formData.imageFile && (
+          <Box mt={2} sx={{ display: "flex", alignItems: "center", justifyContent:"space-evenly" ,flexDirection: "row" }}>
+            <Box>
+            <Typography variant="h6">Imagen</Typography>
+            <img
+              src={formData.imageFile}
+              alt="Preview"
+              style={{
+                maxWidth: '200px',
+                maxHeight: '200px',
+                height: 'auto',
+                width: 'auto',
+                marginTop: 10,
+              }}
+            />
+
+            </Box>
+            {qrValue && (
+          <Box mt={3} >
+            <Typography variant="h6">QR Code</Typography>
+            <Box id="qr-code" mt={1} display="flex" justifyContent="center">
+              <QRCodeCanvas value={qrValue} size={150} />
+            </Box>
+          </Box>
+        )}
+          </Box>
+        )}
+
+        <Box mt={2} display="flex" justifyContent="space-between">
+          <Button variant="contained" color="primary" onClick={generateQR}>
+            Generar QR
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleGeneratePDF}
+          >
+            Descargar PDF
+          </Button>
+        </Box>
+
+        
+      </Paper>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/utils/generatePDF.js
@@ -0,0 +1,122 @@
+import { jsPDF } from "jspdf";
+import cursos29_logo from "../assets/cursos29_logo.png";
+import firmaPNG from "../assets/firma.png";
+import { font } from "../assets/fonts/Montserrat-Regular-normal";
+
+export const generatePDF = (qrValue, formData) => {
+    console.log("QR Value:", qrValue);
+    console.log("Form Data:", formData);
+    console.log("Apellido:", formData.apellido);
+    
+    
+    
+    if (!qrValue) return;
+
+    const pdf = new jsPDF("portrait", "cm", "a4");
+
+    // Add Montserrat-Regular font to the VFS (replace this with your actual base64-encoded font string)
+    const ibmPlexSansBold = `<base64-encoded-font>`; // Replace with your actual base64 data
+
+    pdf.addFileToVFS("IBMPlexSans-Bold.ttf", ibmPlexSansBold);
+
+    pdf.addFileToVFS("Montserrat-Regular.ttf", font);
+    pdf.addFont("Montserrat-Regular.ttf", "Montserrat", "normal");
+    
+
+    // Set font size
+    pdf.setFontSize(16);
+
+    // Add QR Code
+    const qrCanvas = document.getElementById("qr-code").querySelector("canvas");
+    if (qrCanvas) {
+      const qrImage = qrCanvas.toDataURL("image/png");
+      pdf.addImage(qrImage, "PNG", 17.66, 1.71, 1.46, 1.46);
+    }
+
+    // Set bold for specific text
+    pdf.setFontSize(5);
+    pdf.text(`ESCANEAR QR`, 17.67, 3.47);
+
+    // Add logo
+    pdf.addImage(cursos29_logo, "PNG", 8.52, 2.63, 3.94, 3.94);
+
+    // Add Image (either from URL or File)
+    pdf.addImage(formData.imageFile, "JPEG", 70, 70, 50, 50);
+
+    // Set font and size for the certificate title
+    pdf.setFont("Courier", "bold"); // Using normal (non-bold) for regular text
+
+    pdf.setFontSize(19);
+    pdf.text("CERTIFICADO DE APROBACION", 5.59, 7.93);
+
+    // Set Montserrat-Regular font and size for the name
+    pdf.setFont("Montserrat", "normal"); // Using normal (non-bold) for regular text
+    pdf.setFontSize(13);
+    pdf.text(formData.apellido.toUpperCase() + ", " + formData.nombre.toUpperCase(), 7.15, 8.81);
+
+    pdf.setFont("IBMPlexSans-Bold");
+    pdf.setFontSize(13);
+    pdf.text(formData.dni, 9.45, 9.52);
+
+    pdf.setFont("undefined", "normal"); // Using normal (non-bold) for regular text
+    pdf.setFontSize(19);
+    pdf.text("A quien corresponda:", 1.08, 12.53);
+
+    pdf.setFont("Montserrat", "normal"); // Using normal (non-bold) for regular text
+    pdf.setFontSize(15);
+    pdf.text("Mediante el presente Certificado de Aprobación se deja constancia que", 1.04, 14.27, {maxWidth: 19, align: 'justify'} );
+    pdf.text("completó satisfactoriamente el curso teórico-práctico de “OPERACIÓN", 1.04, 14.90, {maxWidth: 19, align: 'justify'} );
+    pdf.text("SEGURA DE AUTOELEVADORES” acorde a la Resolución 960/15 S.R.T.", 1.04, 15.50, {maxWidth: 19, align: 'justify'} );
+    pdf.text("desarrollado por nuestro Centro de Capacitación, donde se extiende el", 1.04, 16, {maxWidth: 19, align: 'justify'} );
+    pdf.text("carnet correspondiente de 1 año de validez.", 1.04, 16.40, {maxWidth: 19, align: 'justify'} );
+
+
+    pdf.text("Atentamente,", 1.11, 18.42);
+
+    pdf.addImage(firmaPNG, "JPEG", 16.17, 19.65, 2.59, 3.33);
+
+    pdf.setFont("undefined", "bold");
+    
+    pdf.text("Alan Gastón Guerrero", 14.33, 23.17);
+    
+    pdf.setFont("undefined", "normal");
+
+    pdf.text("Lic. en Higiene y Seguridad en el Trabajo", 10.5, 23.75);
+    pdf.text("CPSH LHS-000389 PBA", 14.1, 24.33);
+    pdf.text("Instructor", 17.32, 24.87);
+
+    // Footer
+    pdf.setFontSize(12);
+    pdf.setFont("undefined", "bold");
+    pdf.text("www.cursos29.com.ar", 6.49, 27.6);
+    pdf.setFont("undefined", "normal");
+    pdf.text("| 0810 220 1029", 11.26, 27.6);
+    pdf.text("Whatsapp Comercial: 1158059750", 7.43, 27.95);
+    // Footer
+
+    pdf.addPage();
+
+    pdf.setLineWidth(0.03); // Makes the border thinner
+    pdf.setDrawColor(200, 200, 200); // Sets a light grey color
+    pdf.roundedRect(2.1, 4.25, 16.62, 5.4, 0.15, 0.15, "S"); // Draw the rectangle
+
+    // Footer
+    pdf.setFontSize(12);
+    pdf.setFont("undefined", "bold");
+    pdf.text("www.cursos29.com.ar", 6.49, 27.6);
+    pdf.setFont("undefined", "normal");
+    pdf.text("| 0810 220 1029", 11.26, 27.6);
+    pdf.text("Whatsapp Comercial: 1158059750", 7.43, 27.95);
+    // Footer
+
+
+
+
+
+
+
+
+
+    // Save the PDF
+    pdf.save("user_qr.pdf");
+  };
